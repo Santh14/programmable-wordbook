2b524c26f8e57e3f8fbdf824f3dfe92e7af3619e
Santh14 <97513842+Santh14@users.noreply.github.com>
2025-02-21 20:39:13 +0900

    quiz class divided
    WordManager 클래스를 추가하였다. Quiz중에서 문제를 내는 역할에 맞지 않는 메소드를 분할했다.

--- jsonManager.js
+++ jsonManager.js
@@ -1,3 +1,31 @@
+class WordManager {
+    constructor() {
+        this.quizData = {};
+
+        this.jsonFileInput = null;
+    }
+
+    getQuiz() {
+        /* get user's filter and send keys to Quiz object
+            suppose the filter function is implemented named 'filterData'
+            now, beacuse the filterData is not implemented yet,
+                return shuffled keys that have tags empty.
+         */
+
+        const retData = {};
+        shuffle(Object.keys(this.quizData)).map((key) => {
+            if (this.quizData[key]['tag'].length === 0) {
+                retData[key] = this.quizData[key];
+            }
+        });
+        return retData;
+    }
+
+    onload() {
+        this.jsonFileInput = document.getElementById('fileInput');
+    }
+}
+
 class Quiz {
     constructor() {
         this.score = 0;
@@ -5,29 +33,40 @@ class Quiz {
         this.quizData = {};
         this.quizDataKeys = [];
         this.isCorrect = [];
-        this.currentState = 'start';
-        // state: 'start', 'question', 'answer', 'end'
+        this.currentState = 'question';
+        // state: 'question', 'answer', 'end'
 
         this.correctKey = 'Enter';
         this.incorrectKey = 'q';
 
-        this.jsonFileInput = null;
+        this.fileNotUploadedMessageElement = null;
         this.questionWordElement = null;
         this.userAnswerElement = null;
         this.correctAnswerElement = null;
         this.wordTestContainer = null;
+        this.wordTestResult = null;
     }
     
     start() {
+        this.quizData = wordManager.getQuiz();
+        this.quizDataKeys = Object.keys(this.quizData);
+        this.isCorrect = new Array(this.quizDataKeys.length).fill(false);
         this.currentQuestionIndex = 0;
         this.score = 0;
-        this.isCorrect = new Array(this.quizDataKeys.length).fill(false);
-        this.quizDataKeys = Object.keys(this.quizData);
-
+        
+        this.fileNotUploadedMessageElement.style.display = 'none';
         this.wordTestContainer.style.display = 'block';
-        this.jsonFileInput.style.display = 'none';
 
         this.displayWord();
+    }
+
+    end() {
+        // Todo
+        // return results to wordManager
+
+        this.wordTestContainer.style.display = 'none';
+        this.wordTestResult.style.display = 'block';
+        this.wordTestResult.textContent = `Your score is ${this.score}/${this.quizDataKeys.length}.`;
     }
 
     getCurrentQuestion() {
@@ -50,6 +89,9 @@ class Quiz {
 
     nextQuestion() {
         this.currentQuestionIndex++;
+        if (this.currentQuestionIndex === this.quizDataKeys.length) {
+            this.end();
+        }
     }
 
     increaseScore() {
@@ -80,33 +122,40 @@ class Quiz {
     }
 
     onload() {
+        this.fileNotUploadedMessageElement = document.getElementById('fileNotUploadedMessage');
         this.questionWordElement = document.getElementById('questionWord');
         this.userAnswerElement = document.getElementById('userAnswer');
         this.correctAnswerElement = document.getElementById('correctAnswer');
         this.wordTestContainer = document.getElementById('wordTestContainer');
-        this.jsonFileInput = document.getElementById('fileInput');
+        this.wordTestResult = document.getElementById('wordTestResultContainer');
 
         this.wordTestContainer.style.display = 'none';
     }
 }
 
 const quiz = new Quiz();
+const wordManager = new WordManager();
 
-function page2eventHandler() {
+function eventHandler() {
+    // page1 event handler
     document.getElementById('fileInput').addEventListener('change', (event) => {
         const file = event.target.files[0];
         if (file) {
             const reader = new FileReader();
             reader.onload = (e) => {
                 const contents = e.target.result;
-                quiz.quizData = JSON.parse(contents);
-                quiz.start();
+                wordManager.quizData = JSON.parse(contents);
             };
-
             reader.readAsText(file);
+            document.getElementById('startTest').style.display = 'block';
         }
     });
 
+    document.getElementById('startTest').addEventListener('click', () => {
+        quiz.start();
+    });
+
+    // page2 event handler
     document.addEventListener('keydown', function(event) {
         if (quiz.currentState === 'question' && event.key === 'Enter') {
             quiz.displayCorrectAnswer();
@@ -117,12 +166,18 @@ function page2eventHandler() {
             event.preventDefault();
             quiz.setIncorrect();
         }
-    });  
+    });
+}
 
-    quiz.userAnswerElement.addEventListener('input', quiz.displayCorrectAnswer);
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 window.onload = function() {
     quiz.onload();
-    page2eventHandler();
+    eventHandler();
 };
